929776eb9c53e3299886103a5afe1a4f5a9cf3e3
OMSAH <huncyoms@gmail.com>
2024-03-14 08:01:42 -0400

    commited

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,7 +8,7 @@ export default function About() {
       <h2>About Us</h2>
       <img src={imageAbout} alt='aboutimage' />
       <div className='content'>
-        <p>Welcome to my corner of the web! I'm Om Sah, a passionate Computer Science Engineering student and a dedicated full-stack developer. With a curious mind and a love for coding, I'm on a journey to explore the endless possibilities of technology. Join me as I strive to create innovative solutions that bridge the gap between imagination and reality.</p>
+        <p>I'm Om Sah, a passionate Computer Science Engineering student and a dedicated full-stack developer. With a curious mind and a love for coding, I'm on a journey to explore the endless possibilities of technology. Join me as I strive to create innovative solutions that bridge the gap between imagination and reality.</p>
       </div>
     </div>
   )
