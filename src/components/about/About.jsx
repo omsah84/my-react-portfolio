11024b0e1a89f466b014011ae7e49899d95834db
OMSAH <huncyoms@gmail.com>
2024-03-14 07:48:28 -0400

    commited

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './about.scss'
+import imageAbout from './imageAbout.png'
 
 export default function About() {
   return (
     <div className='about' id='about'>
-      
+      <h2>About Us</h2>
+      <img src={imageAbout} alt='aboutimage' />
+      <div className='content'>
+        <p>Welcome to my corner of the web! I'm Om Sah, a passionate Computer Science Engineering student and a dedicated full-stack developer. With a curious mind and a love for coding, I'm on a journey to explore the endless possibilities of technology. Join me as I strive to create innovative solutions that bridge the gap between imagination and reality.</p>
+      </div>
     </div>
   )
 }
