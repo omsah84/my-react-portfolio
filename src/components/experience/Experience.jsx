36db887ac6c9d779ed9ecb0b4f74933c655a0017
OMSAH <huncyoms@gmail.com>
2024-03-15 01:49:27 -0400

    commited

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -5,7 +5,7 @@ export default function Experience() {
   return (
     <div className='experience' id='experience'>
       <div className="heading"><h2> Experience</h2></div>
-      <div></div>
+      <div>hello this is omsah</div>
     </div>
   )
 }
