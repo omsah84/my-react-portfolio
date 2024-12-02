54491a03b83df9961a45920993178778cce6eab8
OMSAH <huncyoms@gmail.com>
2024-12-02 11:54:31 -0500

    added

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
   return (
     <div className='footer'>
       <div class="copyright">
-        <p> Copyright Om Sah, Made with <FaHeart className='text-danger' /> by Om Prakash Sah</p>
+        <p> Copyright Om Sah, Made with <FaHeart className='text-danger' /> by Om Sah</p>
       </div>
     </div>
   )
